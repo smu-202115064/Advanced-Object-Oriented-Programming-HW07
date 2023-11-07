11001248a5775d1814a9401165d6331be7f3461c
Hepheir <hepheir@gmail.com>
2023-11-07 20:51:33 +0900

    Create PasswordInfo

---
+++ src/PasswordInfo.java
@@ -0,0 +1,40 @@
+public class PasswordInfo {
+    private String url;
+    private String id;
+    private String password;
+
+    PasswordInfo(String url, String id, String pw) {
+        this.url = url;
+        this.id = id;
+        this.password = pw;
+    }
+
+    @Override
+    public String toString() {
+        return "" + url + ", " + id + ", " + password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setName(String url) {
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String pw) {
+        this.password = pw;
+    }
+}
