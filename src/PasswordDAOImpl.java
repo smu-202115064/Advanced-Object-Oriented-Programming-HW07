dfc395060afb72229cf568e08796766856b58393
Hepheir <hepheir@gmail.com>
2023-11-07 21:08:00 +0900

    Create PasswordDAOImpl

---
+++ src/PasswordDAOImpl.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+import java.util.List;
+
+public class PasswordDAOImpl implements PasswordDAO {
+    @Override
+    public void insert(PasswordInfo p) {
+
+    }
+
+    @Override
+    public List<PasswordInfo> findAll() {
+        return null;
+    }
+
+    @Override
+    public PasswordInfo findByKey(String url) {
+        return null;
+    }
+
+    @Override
+    public void update(PasswordInfo p) {
+
+    }
+
+    @Override
+    public void delete(String url) {
+
+    }
+
+    @Override
+    public void delete(PasswordInfo p) {
+
+    }
+}
