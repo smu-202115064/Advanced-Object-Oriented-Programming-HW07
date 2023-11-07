6047dc90be1f1a630cd6e70a542a403aef87f9e4
Hepheir <hepheir@gmail.com>
2023-11-07 20:53:48 +0900

    중복된 접근 제한자 제거

--- src/PasswordDAO.java
+++ src/PasswordDAO.java
@@ -1,10 +1,10 @@
 import java.util.List;
 
 public interface PasswordDAO {
-    public void insert(PasswordInfo p);
-    public List<PasswordInfo> findAll();
-    public PasswordInfo findByKey(String url);
-    public void update(PasswordInfo p);
-    public void delete(String url);
-    public void delete(PasswordInfo p);
+    void insert(PasswordInfo p);
+    List<PasswordInfo> findAll();
+    PasswordInfo findByKey(String url);
+    void update(PasswordInfo p);
+    void delete(String url);
+    void delete(PasswordInfo p);
 }
