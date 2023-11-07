6c4a5c85da92f7d6d89e8e0d5b70ba3f8bc8c242
Hepheir <hepheir@gmail.com>
2023-11-07 21:25:14 +0900

    PasswordInfo에 Builder 패턴 적용

--- src/PasswordInfo.java
+++ src/PasswordInfo.java
@@ -1,4 +1,33 @@
 public class PasswordInfo {
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String url;
+        private String id;
+        private String password;
+
+        public Builder setUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public PasswordInfo build() {
+            return new PasswordInfo(this);
+        }
+    }
+
     private String url; // Primary Key
     private String id;
     private String password;
@@ -7,6 +36,12 @@ public class PasswordInfo {
         this.url = url;
         this.id = id;
         this.password = pw;
+    }
+
+    PasswordInfo(Builder builder) {
+        this.url = builder.url;
+        this.id = builder.id;
+        this.password = builder.password;
     }
 
     @Override
